b66b771b7ad899d4952342f46f44128c536532c8
billwestfall <billwestfall@yahoo.com>
2015-07-28 16:07:49 -0700

    Create tutorial02.pl

---
+++ tutorial02.pl
@@ -0,0 +1,18 @@
+% Query like adjacent(Z,1). , then hit ; to get next result
+
+adjacent(1,2).
+adjacent(2,1).
+adjacent(1,3).
+adjacent(3,1).
+adjacent(1,4).
+adjacent(4,1).
+adjacent(1,5).
+adjacent(5,1).
+adjacent(2,3).
+adjacent(3,2).
+adjacent(2,4).
+adjacent(4,2).
+adjacent(3,4).
+adjacent(4,3).
+adjacent(4,5).
+adjacent(5,4).
