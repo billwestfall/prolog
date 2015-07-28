45ae254f253b8b1ea1c7635403e0bc0cb10661e9
billwestfall <billwestfall@yahoo.com>
2015-07-28 16:00:31 -0700

    Create tutorial01.pl

---
+++ tutorial01.pl
@@ -0,0 +1,18 @@
+% This buffer is for notes you don't want to save.
+% If you want to create a file, visit that file with C-x C-f,
+% then enter the text in that file's own buffer.
+% output looks like the following:
+% 1 ?- bigger(_,elephant).
+% true.
+% 2 ?- bigger(X,elephant).
+% X = whale.
+
+
+bigger(whale,elephant).
+bigger(elephant,horse).
+bigger(horse,donkey).
+bigger(donkey,monkey).
+bigger(donkey,dog).
+bigger(dad,mom).
+bigger(mom,chris).
+bigger(chris,stacy).
