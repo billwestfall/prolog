4aa030d85425233479cfb143e13e420da3381f31
billwestfall <billwestfall@yahoo.com>
2015-08-03 21:38:43 -0700

    Create book07b.pl

---
+++ book07b.pl
@@ -0,0 +1,20 @@
+/* example like
+2 ?- likes(mary,france).
+false.
+
+3 ?- athenian(socrates).
+true.
+
+4 ?- athenian(aristotle).
+false.
+
+5 ?- king(john,france).
+ERROR: toplevel: Undefined procedure: king/2 (DWIM could not correct goal)
+
+*/
+
+
+
+human(socrates).
+human(aristotle).
+athenian(socrates).
