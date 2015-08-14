53cf3497156255bcb0848f066f45937c674d7af1
billwestfall <billwestfall@yahoo.com>
2015-08-13 20:02:09 -0700

    Create book52a.pl

---
+++ book52a.pl
@@ -0,0 +1,22 @@
+/* run as
+
+1 ?- p([X|Y]).
+X = 1,
+Y = [2, 3]
+X = the,
+Y = [cat, sat, [on, the, mat]].
+
+2 ?- p([_,_,_,[|X]]).
+ERROR: Syntax error: double_bar_outside_quasiquotation
+ERROR: p([_,_,_
+ERROR: ** here **
+ERROR: ,[|X]]) .
+2 ?- p([_,_,_,[_|X]]).
+X = [the, mat].
+
+3 ?-
+
+*/
+
+p([1,2,3]).
+p([the,cat,sat,[on,the,mat]]).
