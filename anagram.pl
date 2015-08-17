bb7821e3d10eb2ae212f0f7beabaecec7006e95d
billwestfall <billwestfall@yahoo.com>
2015-08-16 20:28:45 -0700

    Create anagram.pl

---
+++ anagram.pl
@@ -0,0 +1,16 @@
+/* from http://www.anselm.edu/homepage/mmalita/culpro/anagram.html run as
+
+2 ?- anagram.
+Write a word=grep.
+
+*/
+
+anagram:-   write('Write a word='),read(X),nl,
+			  name(X,L),permut(L,R),
+		          name(Cuv,R),write(Cuv),tab(5),fail.
+
+		add(X,L,[X|L]).
+		add(X,[L|H],[L|R]):- add(X,H,R).
+
+		permut([],[]).
+		permut([L|H],R):- permut(H,R1),add(L,R1,R).
