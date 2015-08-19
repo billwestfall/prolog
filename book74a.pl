c927733751ea7dc69a1a065c418392e0e429ef48
billwestfall <billwestfall@yahoo.com>
2015-08-19 09:25:19 -0700

    Create book74a.pl

---
+++ book74a.pl
@@ -0,0 +1,15 @@
+/* run as
+
+1 ?- father(sue, N).
+N = harry.
+
+2 ?- father(N, george).
+N = mary
+N = john.
+
+*/
+
+father(mary, george).
+father(john, george).
+father(sue, harry).
+father(george, edward).
