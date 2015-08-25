15c34a51d4159aec26f6c957790c4687d53cee5c
billwestfall <billwestfall@yahoo.com>
2015-08-24 18:00:50 -0700

    Create book93a.pl

---
+++ book93a.pl
@@ -0,0 +1,10 @@
+
+
+line(b(X,Y,Z,_,_,_,_,_,_),X,Y,Z).
+line(b(_,_,_,X,Y,Z,_,_,_),X,Y,Z).
+line(b(_,_,_,_,_,_,X,Y,Z),X,Y,Z).
+line(b(X,_,_,Y,_,_,Z,_,_),X,Y,Z).
+line(b(_,X,_,_,Y,_,_,Z,_),X,Y,Z).
+line(b(_,_,X,_,_,Y,_,_,Z),X,Y,Z).
+line(b(X,_,_,_,Y,_,_,_,Z),X,Y,Z).
+line(b(_,_,X,_,Y,_,Z,_,_),X,Y,Z).
