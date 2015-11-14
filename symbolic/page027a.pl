1c4d3bee532b732bd7c4baa000330355034076a9
Bill Westfall <billwestfall@yahoo.com>
2015-11-13 19:42:56 -0800

    Create page027a.pl
    proving that some old, English books exist

---
+++ symbolic/page027a.pl
@@ -0,0 +1,20 @@
+/*
+MacBook-Air:~ billw$ gprolog --consult-file /Users/billw/Documents/old/prolog/sym01/page027a.pl
+GNU Prolog 1.4.4 (64 bits)
+Compiled Nov 13 2015, 19:00:39 with clang
+By Daniel Diaz
+Copyright (C) 1999-2013 Daniel Diaz
+compiling /Users/billw/Documents/old/prolog/sym01/page027a.pl for byte code...
+/Users/billw/Documents/old/prolog/sym01/page027a.pl compiled, 8 lines read - 764 bytes written, 6 ms
+| ?- books(X,old,english).
+
+X = tempest ? ;
+
+no
+| ?- 
+ */
+
+books(tempest,old,english).
+books(faust,old,non_english).
+books(harry_potter,non_old,english).
+books(little_prince,non_old,non_english).
