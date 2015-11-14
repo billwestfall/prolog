b4a2f92ed6748311e614ef9d8107de654569aee8
Bill Westfall <billwestfall@yahoo.com>
2015-11-13 19:50:04 -0800

    Create page029a.pl
    condition that some x and y, some x are y and some y are x

---
+++ symbolic/page029a.pl
@@ -0,0 +1,23 @@
+MacBook-Air:~ billw$ gprolog --consult-file /Users/billw/Documents/old/prolog/sym01/page029a.pl 
+GNU Prolog 1.4.4 (64 bits)
+Compiled Nov 13 2015, 19:00:39 with clang
+By Daniel Diaz
+Copyright (C) 1999-2013 Daniel Diaz
+compiling /Users/billw/Documents/old/prolog/sym01/page029a.pl for byte code...
+/Users/billw/Documents/old/prolog/sym01/page029a.pl compiled, 8 lines read - 764 bytes written, 5 ms
+| ?- books(X,old,english).
+
+X = tempest ? ;
+
+no
+| ?- books(tempest,X,english).
+
+X = old
+
+yes
+| ?- books(tempest,old,Y).    
+
+Y = english
+
+yes
+| ?- 
