1cae1ad3915262c6911941c8a7b7c3bb430f3dee
Bill Westfall <billwestfall@yahoo.com>
2015-11-13 21:05:31 -0800

    Create page036a.pl
    fulfills the 'some x are y' condition

---
+++ symbolic/page036a.pl
@@ -0,0 +1,25 @@
+/*
+MacBook-Air:~ billw$ gprolog --consult-file /Users/billw/Documents/old/prolog/sym01/page036a.pl
+GNU Prolog 1.4.4 (64 bits)
+Compiled Nov 13 2015, 19:00:39 with clang
+By Daniel Diaz
+Copyright (C) 1999-2013 Daniel Diaz
+compiling /Users/billw/Documents/old/prolog/sym01/page036a.pl for byte code...
+/Users/billw/Documents/old/prolog/sym01/page036a.pl compiled, 10 lines read - 636 bytes written, 6 ms
+| ?- books(X,old,english).
+
+X = tempest ? ;
+
+no
+| ?- books(X,old,non_english).
+
+no
+| ?- 
+ */
+
+ books(tempest,old,english).
+/*
+ books(faust,old,non_english).
+  */
+ books(harry_potter,non_old,english).
+ books(little_prince,non_old,non_english).
