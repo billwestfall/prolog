4bc398728db86ee1990233a2367963dc5dcad9ba
Bill Westfall <billwestfall@yahoo.com>
2015-11-13 19:36:51 -0800

    Create page026b.pl
    creating the condition where no old books exist

---
+++ symbolic/page026b.pl
@@ -0,0 +1,22 @@
+/*
+MacBook-Air:~ billw$ gprolog --consult-file /Users/billw/Documents/old/prolog/sym01/page026b.pl
+GNU Prolog 1.4.4 (64 bits)
+Compiled Nov 13 2015, 19:00:39 with clang
+By Daniel Diaz
+Copyright (C) 1999-2013 Daniel Diaz
+compiling /Users/billw/Documents/old/prolog/sym01/page026b.pl for byte code...
+/Users/billw/Documents/old/prolog/sym01/page026b.pl compiled, 10 lines read - 512 bytes written, 4 ms
+| ?- books(x,old,english).
+
+no
+| ?- books(x,old,non_english).
+
+no
+| ?-
+ */
+
+/* books(tempest,old,english).
+books(faust,old,non_english).
+*/
+books(harry_potter,non_old,english).
+books(little_prince,non_old,non_english).
