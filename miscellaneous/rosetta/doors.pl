b79bb179e1c8a9d87fa8781f364558ae29c45333
Bill Westfall <billwestfall@yahoo.com>
2017-05-20 12:17:29 -0700

    Update doors.pl

--- miscellaneous/rosetta/doors.pl
+++ miscellaneous/rosetta/doors.pl
@@ -2,9 +2,8 @@
 % run as consult('doors.pl').
 
 doors(0):-!.
-prize(0):-random(99).
+prize(Y):-random(99).
 doors(X):-
-prize(Y):-
     X =:= Y ->
     writef('%t The door with the prize is \n',[X]),
     doors(XN).
