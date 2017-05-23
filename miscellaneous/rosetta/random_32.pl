11e01c5a7f96f6420eeac9d790cbdf09e5a0ea38
Bill Westfall <billwestfall@yahoo.com>
2017-05-23 16:31:01 -0700

    Update random_32.pl

--- miscellaneous/rosetta/random_32.pl
+++ miscellaneous/rosetta/random_32.pl
@@ -1,6 +1,13 @@
 /**
 * generates a random 32 bit number , to the maximum value in 32 bit
+* run as 
+*?- random_between(0, 2147483647, X).
+*X = 489556727.
+
+*?- random_between(0, 2147483647, X).
+*X = 197484725.
+ 
 */
 
-random_between(0, 2147483647, X).
-write(X).
+
+
